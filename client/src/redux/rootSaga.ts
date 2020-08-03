bae120e4d2945eff65bea1a695ff428eb9fb9a68
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-08-03 23:15:36 +0300

    Add project reducer, saga, state to root

--- client/src/redux/rootSaga.ts
+++ client/src/redux/rootSaga.ts
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import exampleSaga from 'containers/Example/logic/saga';
+import projectsSaga from 'containers/Projects/logic/saga';
 
 export default function* rootSaga() {
-	yield all([exampleSaga()]);
+	yield all([exampleSaga(), projectsSaga()]);
 }
--- client/src/typings/rootState.ts
+++ client/src/typings/rootState.ts
@@ -1,5 +1,7 @@
+import { ProjectsState } from '../containers/Projects/logic/state';
 import { ExampleState } from 'containers/Example/logic/state';
 
 export interface RootState {
 	example: ExampleState;
+	projects: ProjectsState;
 }
