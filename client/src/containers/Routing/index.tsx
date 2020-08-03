b6a1f568b3f6c31dc4d7f2a0b0fb12dcc3598b6d
Tima Gixe <timagixe@protonmail.com>
2020-08-03 18:32:45 +0300

    Add Login to existing routes

--- client/src/containers/Routing/index.tsx
+++ client/src/containers/Routing/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import Example from 'containers/Example';
+import Login from '../../pages/Login';
 import PublicRoute from 'components/PublicRoute';
 
 interface Props {}
 
-const Routing: React.FC<Props> = () => (
+const Routing: React.FC<Props> = (props) => (
 	<Switch>
 		<PublicRoute exact restricted={false} path="/" component={Example} />
+		<PublicRoute exact restricted={false} path="/login" component={Login} />
 	</Switch>
 );
 
