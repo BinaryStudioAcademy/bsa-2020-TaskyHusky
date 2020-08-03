635fd44414d2afb0b945eefaa1d04a8f6fd164ba
kostia1359 <tzimbal.k@gmail.com>
2020-08-03 20:23:20 +0300

    changed migration

--- server/src/migration/1596468583404-BoardColumns.ts
+++ server/src/migration/1596468583404-BoardColumns.ts
@@ -9,14 +9,21 @@ export class BoardColumns1596468583404 implements MigrationInterface {
 		const columnRepository = getRepository('BoardColumn');
 
 		const board = await boardRepository.findByType('first');
-		const column1 = new BoardColumn();
 
+		const column1 = new BoardColumn();
 		column1.board = board;
 		column1.columnName='one';
 		column1.isResolutionSet=false;
 		column1.status='unknown';
 
+		const column2 = new BoardColumn();
+		column2.board = board;
+		column2.columnName='two';
+		column2.isResolutionSet=true;
+		column2.status='unknown';
+
 		await columnRepository.save(column1);
+		await columnRepository.save(column2);
 		await getRepository('board').save(board);
 	}
 
