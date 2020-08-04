f8d91a2ad59e35ff67024f23f70860dbcd8121c5
R-Mielamud <2m.roman2@gmail.com>
2020-08-04 12:09:46 +0300

    Add priority crud

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import example from './example.routes';
 import auth from './auth.routes';
 import issueType from './issueType.routes';
+import priority from './priority.routes';
 
 const routes = Router();
 
 routes.use('/example', example);
 routes.use('/auth', auth);
 routes.use('/issue_type', issueType);
+routes.use('/priority', priority);
 
 export default routes;
