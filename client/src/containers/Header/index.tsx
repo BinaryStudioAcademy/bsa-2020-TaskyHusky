752e1683d614ac43fa6bc39818350799037e96a4
Tima Gixe <timagixe@protonmail.com>
2020-08-04 14:56:15 +0300

    Add dropdowns

--- client/src/containers/Header/index.tsx
+++ client/src/containers/Header/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Menu, Image, Input, Button } from 'semantic-ui-react';
+import { Menu, Image, Input, Button, Dropdown, Icon } from 'semantic-ui-react';
 import logo from 'assets/logo192.png'; // TODO: replace with logo once it is ready
 import styles from './styles.module.scss';
 import { Redirect } from 'react-router-dom';
@@ -33,25 +33,52 @@ export const HeaderMenu = () => {
 					Your work
 				</Menu.Item>
 
-				<Menu.Item
-					name="projects"
-					active={activeItem === 'projects'}
-					onClick={() => toggleActiveItem('projects')}
-				>
-					Projects
-				</Menu.Item>
+				<Dropdown text="Projects" className="link item">
+					<Dropdown.Menu>
+						<Dropdown.Header>Recent</Dropdown.Header>
+						<Dropdown.Item>
+							<Icon name="folder open" />
+							Project #1
+						</Dropdown.Item>
+						<Dropdown.Item>
+							<Icon name="folder open" />
+							Project #1
+						</Dropdown.Item>
+						<Dropdown.Divider />
+						<Dropdown.Item>View all projects</Dropdown.Item>
+						<Dropdown.Item>Create project</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
 
-				<Menu.Item name="filters" active={activeItem === 'filters'} onClick={() => toggleActiveItem('filters')}>
-					Filters
-				</Menu.Item>
+				<Dropdown text="Filters" className="link item">
+					<Dropdown.Menu>
+						<Dropdown.Header>Recent</Dropdown.Header>
+						<Dropdown.Item>
+							<Icon name="filter" />
+							Filter #1
+						</Dropdown.Item>
+						<Dropdown.Item>
+							<Icon name="filter" />
+							Filter #1
+						</Dropdown.Item>
+						<Dropdown.Divider />
+						<Dropdown.Item>View all filters</Dropdown.Item>
+						<Dropdown.Item>Advanced issue search</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
 
-				<Menu.Item
-					name="dashboards"
-					active={activeItem === 'dashboards'}
-					onClick={() => toggleActiveItem('dashboards')}
-				>
-					Dashboards
-				</Menu.Item>
+				<Dropdown text="Dashboards" className="link item">
+					<Dropdown.Menu>
+						<Dropdown.Header>Recent</Dropdown.Header>
+						<Dropdown.Item>
+							<Icon name="flipboard" />
+							Dashboard
+						</Dropdown.Item>
+						<Dropdown.Divider />
+						<Dropdown.Item>View all dashboards</Dropdown.Item>
+						<Dropdown.Item>Create dashboard</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
 
 				<Menu.Item name="people" active={activeItem === 'people'} onClick={() => toggleActiveItem('people')}>
 					People
