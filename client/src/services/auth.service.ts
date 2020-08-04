f166377dde285bd5b62c197ba55eb7057a94d27d
Tima Gixe <timagixe@protonmail.com>
2020-08-04 23:30:40 +0300

    Add loginUser helper

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -17,3 +17,21 @@ export const registerUser = async (email: string, password: string): Promise<Web
 
 	return (await res.json()) as WebApi.Result.UserAuthResult;
 };
+
+export const loginUser = async (email: string, password: string): Promise<WebApi.Result.UserLoginResult | null> => {
+	const res = await callWebApi({
+		endpoint: 'auth/login',
+		method: 'POST',
+		body: {
+			email,
+			password,
+		},
+	});
+
+	if (res.status > 399) {
+		// Replace this with error handling (when there will be a handler on server)
+		return null;
+	}
+
+	return (await res.json()) as WebApi.Result.UserLoginResult;
+};
