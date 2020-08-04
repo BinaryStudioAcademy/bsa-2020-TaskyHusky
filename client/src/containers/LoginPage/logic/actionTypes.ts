a125e743dd4833393f3d791c433dead5a8b518e9
Tima Gixe <timagixe@protonmail.com>
2020-08-04 23:32:58 +0300

    Replace type with interface and add jwtToken property

--- client/src/containers/LoginPage/logic/actionTypes.ts
+++ client/src/containers/LoginPage/logic/actionTypes.ts
@@ -1,4 +1,4 @@
-import { AuthState } from './state';
+import { User } from './state';
 
 export const TRIGGER_LOGIN_USER = 'TRIGGER_LOGIN_USER';
 export const UPDATE_LOGIN_USER = 'UPDATE_LOGIN_USER';
@@ -8,6 +8,7 @@ export type TriggerLoginUser = {
 	password: string;
 };
 
-export type UpdateLoginUser = {
-	user: AuthState;
-};
+export interface UpdateLoginUser {
+	jwtToken: string;
+	user: User;
+}
