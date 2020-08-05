4deaa64ea027392ceec22d01bd6ff3f6e2fe233f
Tima Gixe <timagixe@protonmail.com>
2020-08-05 12:11:01 +0300

    Move destructuring to IF block

--- client/src/containers/LoginPage/logic/saga.ts
+++ client/src/containers/LoginPage/logic/saga.ts
@@ -6,11 +6,11 @@ import * as actions from './actions';
 export function* logInRequest(action: ReturnType<typeof actions.triggerLoginUser>) {
 	const { email, password } = action;
 	const response: WebApi.Result.UserLoginResult = yield call(loginUser, email, password);
-	const { user, jwtToken } = response;
 
 	// If there is no user in DB - do not nothing, should be fixed (I guess we need error handler on client side)
 	// Now if there is no user in DB -> server throws an error RangeError [ERR_HTTP_INVALID_STATUS_CODE]: Invalid status code: undefined
 	if (response) {
+		const { user, jwtToken } = response;
 		yield put(actions.updateLoginUser({ user, jwtToken }));
 	}
 }
