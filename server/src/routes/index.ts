bac6fc707404a77886c7348490c1cd5dd05ea09a
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-08-05 04:10:34 +0300

    Add project routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import example from './example.routes';
 import auth from './auth.routes';
+import projects from './projects.routes';
 
 const routes = Router();
 
 routes.use('/example', example);
 routes.use('/auth', auth);
+routes.use('/projects', projects);
 
 export default routes;
