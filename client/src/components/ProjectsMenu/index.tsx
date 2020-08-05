8cc8a82be5a9b1ada33b4cf501bb1ff65f13d6ee
Tima Gixe <timagixe@protonmail.com>
2020-08-05 17:19:07 +0300

    Add dummy ProjectsMenu component

---
+++ client/src/components/ProjectsMenu/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Dropdown, Icon } from 'semantic-ui-react';
+import styles from './styles.module.scss';
+
+export const ProjectsMenu = () => {
+	return (
+		<Dropdown text="Projects" className="link item dropDownItem" id="dropDownProject">
+			<Dropdown.Menu className={styles.dropDownMenu}>
+				<Dropdown.Header>Recent</Dropdown.Header>
+				<Dropdown.Item>
+					<Icon name="folder open" />
+					Project #1
+				</Dropdown.Item>
+				<Dropdown.Item>
+					<Icon name="folder open" />
+					Project #1
+				</Dropdown.Item>
+				<Dropdown.Divider />
+				<Dropdown.Item>View all projects</Dropdown.Item>
+				<Dropdown.Item>Create project</Dropdown.Item>
+			</Dropdown.Menu>
+		</Dropdown>
+	);
+};
+
+export default ProjectsMenu;
