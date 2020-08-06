d5551ac480f7218b490bb35339b7ce341982ab2d
Tima Gixe <timagixe@protonmail.com>
2020-08-06 15:01:29 +0300

    Create Sprint routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import filter from './filter.routes';
 import auth from './auth.routes';
 import projects from './projects.routes';
-
 import board from './board.routes';
 import user from './user.routes';
 import issue from './issue.routes';
+import sprint from './sprint.routes';
 
 const routes = Router();
 
@@ -15,6 +15,7 @@ routes
 	.use('/user', user)
 	.use('/issue', issue)
 	.use('/board', board)
-	.use('/projects', projects);
+	.use('/projects', projects)
+	.use('/sprint', sprint);
 
 export default routes;
