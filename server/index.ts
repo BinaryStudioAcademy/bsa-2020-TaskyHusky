452d9228cbcf350a104f7fef785214b9aac9f411
danylokarpenko <karpenkodanylo@gmail.com>
2020-08-06 13:16:47 +0300

    comment

--- server/index.ts
+++ server/index.ts
@@ -11,20 +11,21 @@ import { routesWhiteList } from './config/jwt.config';
 import { authenticateJwt } from './src/middleware/jwt.middleware';
 import errorHandlerMiddleware from './src/middleware/errorHandler.middleware';
 
-createConnection().then(async (connection) => {
-	await connection.runMigrations();
-	const app = express();
-	app.use(cors());
-	app.use(bodyParser.json());
-	app.use(passport.initialize());
+createConnection()
+	.then(async (connection) => {
+		await connection.runMigrations();
+		const app = express();
+		app.use(cors());
+		app.use(bodyParser.json());
+		app.use(passport.initialize());
 
-	app.use('/api', authenticateJwt(routesWhiteList), routes);
-	app.use(errorHandlerMiddleware)
+		app.use('/api', authenticateJwt(routesWhiteList), routes);
+		app.use(errorHandlerMiddleware);
 
-	app.listen(appPort, () => {
+		app.listen(appPort, () => {
+			// eslint-disable-next-line no-console
+			console.log(`Server is running on port ${appPort}`);
+		});
 		// eslint-disable-next-line no-console
-		console.log(`Server is running on port ${appPort}`);
 	})
-// eslint-disable-next-line no-console
-}).catch((err) => console.log(err));
-
+	.catch((err) => console.log(err));
