6c3a0f78cfd31bd2e57c5ac1978fa1e66e15b94c
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-08-06 23:24:53 +0300

    Add create project service method

--- client/src/services/projects.service.ts
+++ client/src/services/projects.service.ts
@@ -1,4 +1,5 @@
 import callWebApi from './../helpers/callApi.helper';
+import { InitialProject } from './../containers/CreateProjectModal/logic/actionTypes';
 
 export const getProjects = async (): Promise<WebApi.Entities.Projects[]> => {
 	const res: Response = await callWebApi({
@@ -7,4 +8,16 @@ export const getProjects = async (): Promise<WebApi.Entities.Projects[]> => {
 	});
 
 	return (await res.json()) as WebApi.Entities.Projects[];
+};
+
+export const createProject = async (project: InitialProject): Promise<WebApi.Entities.Projects> => {
+	const res: Response = await callWebApi({
+		method: 'POST',
+		endpoint: 'projects',
+		body: {
+			project,
+		},
+	});
+
+	return (await res.json()) as WebApi.Entities.Projects;
 };
