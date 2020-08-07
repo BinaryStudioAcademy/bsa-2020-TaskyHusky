d590758765559746fcff0fe56fd8b7e8e4ad54e2
Tima Gixe <timagixe@protonmail.com>
2020-08-07 18:09:37 +0300

    Update SignUpForm with redux-saga

--- client/src/containers/SignUpForm/index.tsx
+++ client/src/containers/SignUpForm/index.tsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Divider, Icon } from 'semantic-ui-react';
 import PasswordInput from 'components/common/PasswordInput';
 import validator from 'validator';
 import { registerUser } from 'services/auth.service';
 import { setToken } from 'helpers/setToken.helper';
 import { Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from 'containers/LoginPage/logic/actions';
+import { RootState } from 'typings/rootState';
 
 const SignUpForm: React.FC = () => {
+	const dispatch = useDispatch();
+	const authStore = useSelector((rootStore: RootState) => rootStore.auth);
+
 	const [email, setEmail] = useState<string>('');
 	const [emailValid, setEmailValid] = useState<boolean>(true);
 	const [password, setPassword] = useState<string>('');
@@ -19,26 +25,21 @@ const SignUpForm: React.FC = () => {
 
 	const buttonDisabled = !(password && passwordValid && email && emailValid && firstName && firstNameValid);
 
+	useEffect(() => {
+		if (!!authStore.jwtToken) {
+			setToken(authStore.jwtToken);
+			setLoading(false);
+			setRedirecting(true);
+		}
+	}, [authStore.isAuthorized, redirecting]);
+
 	const submit = async () => {
 		if (buttonDisabled) {
 			return;
 		}
 
 		setLoading(true);
-
-		const result: WebApi.Result.UserAuthResult | null = await registerUser({
-			email,
-			password,
-			firstName,
-			lastName,
-		});
-
-		setLoading(false);
-
-		if (result) {
-			setToken(result.jwtToken);
-			setRedirecting(true);
-		}
+		dispatch(actions.registerUserTrigger({ email, password, firstName, lastName }));
 	};
 
 	return (
