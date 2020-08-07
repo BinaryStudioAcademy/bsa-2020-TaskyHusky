b80311e629d1f6fb3dfe8fa533e480887b49be2a
Roman <romansahan4@gmail.com>
2020-08-07 16:04:38 +0300

    Update callApi.helper.ts

--- client/src/helpers/callApi.helper.ts
+++ client/src/helpers/callApi.helper.ts
@@ -3,6 +3,7 @@ import { LocalStorageKeys } from 'constants/LocalStorageKeys';
 import { WebApiException } from 'typings/webApiException';
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL ?? '';
+const API = '/api/';
 
 interface RequestArgs {
 	endpoint: string;
@@ -50,10 +51,8 @@ export async function throwIfResponseFailed(res: Response) {
 	}
 }
 
-const API = '/api/';
-
 const getUrl = (args: RequestArgs): RequestInfo =>
-	REACT_APP_API_BASE_URL + API + args.endpoint + (args.query ? `?${qs.stringify(args.query)}` : '');
+	BASE_URL + API + args.endpoint + (args.query ? `?${qs.stringify(args.query)}` : '');
 
 const getArgs = (args: RequestArgs): RequestInit => {
 	const headers: Headers | string[][] | Record<string, string> | undefined = {};
