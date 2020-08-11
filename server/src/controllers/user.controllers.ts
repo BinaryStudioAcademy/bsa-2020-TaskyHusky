9bf45f1b81a9f12700fe9b9a0f30a45ae76dfdea
Alesya103 <alesya.afanaseva27@gmail.com>
2020-08-12 02:51:24 +0300

    fix update user controller

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -51,8 +51,11 @@ class UserController {
 		if (req.body.email) {
 			const checkUser = await userRepository.getByEmail(req.body.email);
 			if (checkUser && checkUser.id !== id) {
-				throw new Error('Yhis email is already taken');
+				throw new Error('This email is already taken');
 			}
+		}
+		if (req.body.password) {
+			throw new Error('Forbidden to change password');
 		}
 		try {
 			const updatedUser = await userRepository.updateById(id, req.body);
