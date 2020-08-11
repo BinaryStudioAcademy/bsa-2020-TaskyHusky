27edfb4aea36f54ad7bd7134b5d6f7f80616d29e
kostia1359 <tzimbal.k@gmail.com>
2020-08-11 21:11:30 +0300

    bug fixed

--- server/src/middleware/jwt.middleware.ts
+++ server/src/middleware/jwt.middleware.ts
@@ -1,19 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import passport from '../../config/passport.config';
 
-export const authenticateJwt = (routesWhiteList: string[]) => {
-	function isUrlInWhiteList(url: string) {
+function isUrlInWhiteList(url: string, routesWhiteList:string[]) {
 
-		return routesWhiteList.filter((route: string) => {
-			const regExpString = '^'.concat(route).concat('$').replace('*', '.*');
-			const regExp = new RegExp(regExpString);
+	return routesWhiteList.some((route: string) => {
+		const regExpString =`^${route}$`.replace('*', '.*');
+		const regExp = new RegExp(regExpString);
 
-			return regExp.test(url);
-		}).length !== 0;
-	}
+		return regExp.test(url);
+	});
+}
 
-	return (req: Request, res: Response, next: NextFunction) =>
-		isUrlInWhiteList(req.originalUrl)
+export const authenticateJwt = (routesWhiteList: string[]) =>
+	(req: Request, res: Response, next: NextFunction) =>
+		isUrlInWhiteList(req.originalUrl, routesWhiteList)
 			? next()
 			: passport.authenticate('jwt', { session: false })(req, res, next);
-};
+
