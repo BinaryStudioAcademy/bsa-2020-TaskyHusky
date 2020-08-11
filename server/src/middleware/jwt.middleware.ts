6447cad31caa46d26bc36c577561801f3e13822d
kostia1359 <tzimbal.k@gmail.com>
2020-08-11 19:51:09 +0300

    add token route to white list

--- server/src/middleware/jwt.middleware.ts
+++ server/src/middleware/jwt.middleware.ts
@@ -1,8 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import passport from '../../config/passport.config';
 
-export const authenticateJwt = (routesWhiteList: string[]) =>
-    (req: Request, res: Response, next: NextFunction) =>
-        routesWhiteList.includes(req.originalUrl)
-            ? next()
-            : passport.authenticate('jwt', { session: false })(req, res, next);
+export const authenticateJwt = (routesWhiteList: string[]) => {
+	function isUrlInWhiteList(url: string) {
+
+		return routesWhiteList.filter((route: string) => {
+			const regExpString = '^'.concat(route).concat('$').replace('*', '.*');
+			const regExp = new RegExp(regExpString);
+
+			return regExp.test(url);
+		}).length !== 0;
+	}
+
+	return (req: Request, res: Response, next: NextFunction) =>
+		isUrlInWhiteList(req.originalUrl)
+			? next()
+			: passport.authenticate('jwt', { session: false })(req, res, next);
+};
