20d1fdeba25edd55848de58f575bfa0ead856e25
R-Mielamud <2m.roman2@gmail.com>
2020-08-11 14:28:41 +0300

    Add basic structure

---
+++ client/src/containers/ProjectIssuesPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+	projectId: string;
+}
+
+const ProjectIssuesPage: React.FC<Props> = ({ projectId }) => {
+	return <></>;
+};
+
+export default ProjectIssuesPage;
