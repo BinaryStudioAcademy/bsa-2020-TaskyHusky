578984bf1d6f6b2f805de21f950554c1f991999f
kostia1359 <tzimbal.k@gmail.com>
2020-08-12 10:34:13 +0300

    undo removing url whiteList

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ createConnection()
 		app.use(bodyParser.json());
 		app.use(passport.initialize());
 		app.use('/', (req, res, next) => (req.path === '/' ? res.sendStatus(200) : next())); // health check
-		app.use('/api', routes);
+		app.use('/api', authenticateJwt(routesWhiteList), routes);
 		app.use(errorHandlerMiddleware);
 
 		app.listen(appPort, () => {
