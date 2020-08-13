615ba74a7ba95bddedc9981cffa337cfaa29bb37
R-Mielamud <2m.roman2@gmail.com>
2020-08-13 21:18:44 +0300

    Add router

--- server/src/routes/issue.routes.ts
+++ server/src/routes/issue.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import IssueController from '../controllers/issue.controllers';
 import issueType from './issueType.routes';
 import priority from './priority.routes';
+import comment from './issueComment.routes';
 
 const router = Router();
 const controller = new IssueController();
@@ -9,6 +10,7 @@ const controller = new IssueController();
 router
 	.use('/type', issueType)
 	.use('/priority', priority)
+	.use('/comment', comment)
 	.get('/', controller.getAll)
 	.get('/:id', controller.getById)
 	.get('/byKey/:key', controller.getByKey)
