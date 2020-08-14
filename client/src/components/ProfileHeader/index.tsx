944461f836188e785010d61d689ee016276e4460
Alesya103 <alesya.afanaseva27@gmail.com>
2020-08-14 15:17:26 +0300

    add styles to aside

--- client/src/components/ProfileHeader/index.tsx
+++ client/src/components/ProfileHeader/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-const ProfileHeader = () => <div className={styles.header}></div>;
+const ProfileHeader = () => (
+	<div className={styles.header}>
+		<h1 className={styles.header__content}>My Profile</h1>
+	</div>
+);
 
 export default ProfileHeader;
