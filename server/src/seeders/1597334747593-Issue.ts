a6efeb62df3c5a7fa7b90e33551565e806ae4a44
danylokarpenko <karpenkodanylo@gmail.com>
2020-08-14 15:11:30 +0300

    Improve filter logic && update filterDef and Issue seeders

--- server/src/seeders/1597334747593-Issue.ts
+++ server/src/seeders/1597334747593-Issue.ts
@@ -1,4 +1,5 @@
 import { MigrationInterface, QueryRunner, getRepository, getCustomRepository } from 'typeorm';
+import { PriorityRepository } from '../repositories/priority.repository';
 import { UserRepository } from '../repositories/user.repository';
 import { ProjectsRepository } from '../repositories/projects.repository';
 import { IssueTypeRepository } from '../repositories/issueType.repository';
@@ -19,19 +20,42 @@ export class Issue1597334747593 implements MigrationInterface {
 		const issueTypeRepository = getCustomRepository(IssueTypeRepository);
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		const issueType = (await issueTypeRepository.findAll())[0]!;
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const issueType2 = (await issueTypeRepository.findAll())[1]!;
+
+		const priorityRepository = getCustomRepository(PriorityRepository);
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const priority = (await priorityRepository.findAll())[0]!;
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const priority2 = (await priorityRepository.findAll())[1]!;
 
 		const issue = new Issue();
-		issue.type = issueType;
 		issue.project = project;
 		issue.summary = 'Summary about issue';
 		issue.description = 'Description about issue';
 		issue.creator = user1;
 		issue.assigned = user2;
 		issue.labels = '{label}';
-		issue.attachments = '{attach}';
-		issue.links = '{link}';
-		issue.issueKey = '{key}';
-		await getRepository('Issue').save(issue);
+		issue.attachments = '{attachments}';
+		issue.links = '{link/toSpace}';
+		issue.issueKey = 'IK-2';
+		issue.type = issueType;
+		issue.priority = priority;
+
+		const issue2 = new Issue();
+		issue2.project = project;
+		issue2.summary = 'Summary about issue2';
+		issue2.description = 'Description about issue2';
+		issue2.creator = user1;
+		issue2.assigned = user2;
+		issue2.labels = '{label}';
+		issue2.attachments = '{attachments2}';
+		issue2.links = '{link/toSpace}';
+		issue2.issueKey = 'IK-2';
+		issue2.type = issueType2;
+		issue2.priority = priority2;
+
+		await getRepository('Issue').save([issue, issue2]);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {}
