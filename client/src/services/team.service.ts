955de30ee5b72e48838cf9e40ba06dc39926d098
kostia1359 <tzimbal.k@gmail.com>
2020-08-14 07:22:47 +0300

    deleted unused var

--- client/src/services/team.service.ts
+++ client/src/services/team.service.ts
@@ -50,26 +50,6 @@ export const updateFieldById = async (id: string, field: any): Promise<WebApi.En
 	}
 };
 
-const members = [
-	{
-		id: 101,
-		avatar:
-			'https://i7.pngflow.com/pngimage/779/60/png-computer-icons-login-avatar-avatar-heroes-silhouette-user-symbol-clipart.png',
-		name: 'Vladimir Barkalov',
-		position: 'Java developer',
-	},
-	{
-		id: 102,
-		avatar: 'https://images.clipartlogo.com/files/istock/previews/9859/98596917-worker-avatar-icon.jpg',
-		name: 'Yaroslav Pryhoda',
-		position: 'Web developer',
-	},
-];
-
-export const teamMembers = () => {
-	return members;
-};
-
 export const fetchTeams = async () => {
 	const res = await callWebApi({
 		method: 'GET',
