c2a0ff3467f4d8dfae32ff99d5ee3833177682c5
kostia1359 <tzimbal.k@gmail.com>
2020-08-14 06:02:45 +0300

    fixed seed

--- server/src/seeders/1606645895543-Teams.ts
+++ server/src/seeders/1606645895543-Teams.ts
@@ -1,22 +1,30 @@
-import { getRepository, MigrationInterface, QueryRunner } from 'typeorm';
+import { getRepository, MigrationInterface, QueryRunner, getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/user.repository';
 
 export class Team1606645895543 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		// const exampleData = [
-		// 	{
-		// 		name: 'Avengers',
-		// 		description: 'New team for cool projects',
-		// 		links: [
-		// 			{
-		// 				id: '1fff45fd094',
-		// 				http: 'http://localhost:3000',
-		// 				name: 'BSA Jira',
-		// 				description: 'Our cool project',
-		// 			},
-		// 		],
-		// 	},
-		// ];
-		// await getRepository('Team').save(exampleData);
+		const userRepository = getCustomRepository(UserRepository);
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const user = (await userRepository.getByEmail('test@test.com'))!;
+
+		const exampleData = [
+			{
+				name: 'Avengers',
+				description: 'New team for cool projects',
+				links: [
+					{
+						id: '1fff45fd094',
+						http: 'http://localhost:3000',
+						name: 'BSA Jira',
+						description: 'Our cool project',
+					},
+				],
+				createdBy:user,
+				color:'red',
+
+			},
+		];
+		await getRepository('Team').save(exampleData);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> { }
