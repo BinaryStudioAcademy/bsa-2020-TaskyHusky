d1a64ae059144fbbdb93cb69f0c04ec27c1428bc
Alesya103 <alesya.afanaseva27@gmail.com>
2020-08-17 12:25:16 +0300

    add fixes to classes

--- client/src/components/EmailManager/index.tsx
+++ client/src/components/EmailManager/index.tsx
@@ -40,8 +40,8 @@ const EmailManager: React.FC<Props> = (props: Props) => {
 		<section className={styles.container}>
 			<h3 className={styles.header}>{t('email')}</h3>
 			<div className={styles.card}>
-				<h4 className={styles.card__header}>{t('change_email')}</h4>
-				<p className={styles.card__textData}>
+				<h4 className={styles.cardHeader}>{t('change_email')}</h4>
+				<p className={styles.textData}>
 					{t('current_email')}
 					{email}
 				</p>
@@ -68,14 +68,14 @@ const EmailManager: React.FC<Props> = (props: Props) => {
 						{t('save_changes')}
 					</Button>
 				</Form>
-				<h4 className={styles.card__header}>{t('email_notif')}</h4>
-				<p className={styles.card__textData}>{t('content_notif')}</p>
+				<h4 className={styles.cardHeader}>{t('email_notif')}</h4>
+				<p className={styles.textData}>{t('content_notif')}</p>
 				<Select
 					placeholder={t('choose_option_notif')}
 					className={styles.select}
 					options={notifaictionsOptions}
 				/>
-				<p className={styles.card__textData}>{t('get_email_when')}</p>
+				<p className={styles.textData}>{t('get_email_when')}</p>
 				<Checkbox className={styles.checkbox} label={t('watching_issue')} />
 				<Checkbox className={styles.checkbox} label={t('you_reporter')} />
 				<Checkbox className={styles.checkbox} label={t('you_asignee')} />
