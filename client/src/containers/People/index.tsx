d3e1e3e826385bc55c3fe590ab6638b3a381d403
Alesya103 <alesya.afanaseva27@gmail.com>
2020-08-18 15:20:00 +0300

    fixes

--- client/src/containers/People/index.tsx
+++ client/src/containers/People/index.tsx
@@ -24,6 +24,7 @@ const People: React.FC = (): ReactElement => {
 
 	useEffect((): void => {
 		dispatch(actions.startLoading({ id: authStore.user?.id || '' }));
+		//eslint-disable-next-line
 	}, [dispatch]);
 
 	const redirectToPersonProfile = (id: string) => {
