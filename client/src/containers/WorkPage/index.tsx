dc489b6aad3fe09bdbb4483d4dda75b6ea260324
Alesya103 <alesya.afanaseva27@gmail.com>
2020-08-18 14:02:56 +0300

    fix work page header

--- client/src/containers/WorkPage/index.tsx
+++ client/src/containers/WorkPage/index.tsx
@@ -21,7 +21,7 @@ const WorkPage = () => {
 				<Spinner />
 			) : (
 				<div className={styles.wrapper}>
-					<ProfileHeader title={t('my-work')} />
+					<ProfileHeader title={t('my_work')} />
 					<div className={styles.container}>
 						<ManagerWorkBlock changeMode={changeMode} modeToShow={modeToShow} />
 						<ManagerWorkSection projects={projects} modeToShow={modeToShow} />
