b41ea37497c8d6759ea3e2e6ef47ddde7e218d79
kostia1359 <tzimbal.k@gmail.com>
2020-08-21 10:51:19 +0300

    fixed problem with 2 routes

--- client/src/services/people.service.ts
+++ client/src/services/people.service.ts
@@ -2,10 +2,9 @@ import callWebApi from '../helpers/callApi.helper';
 import { modifiedEntity } from '../containers/SearchPeopleAndTeamField/logic/state';
 
 export const fetchPeople = async (id: string) => {
-	console.log(`user/${id}/teammates`);
 	const res = await callWebApi({
 		method: 'GET',
-		endpoint: `user/${id}/teammatesTemp`,
+		endpoint: `user/${id}/teammates`,
 	});
 
 	return (await res.json()) as WebApi.Entities.UserProfile[];
