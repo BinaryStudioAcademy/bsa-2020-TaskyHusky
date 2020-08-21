dc20d9376ca4c79ea62e8b095f7ae7e83cb1012c
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-08-21 06:41:41 +0300

    Add project trash into context menu on prodjects list page

--- client/src/containers/Projects/index.tsx
+++ client/src/containers/Projects/index.tsx
@@ -1,21 +1,31 @@
 import React, { useState, ChangeEvent } from 'react';
 import { Input, Table } from 'semantic-ui-react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'typings/rootState';
-import Options from './../../components/common/Options';
 import CreateProjectModal from '../CreateProjectModal';
 import styles from './styles.module.scss';
 import Spinner from 'components/common/Spinner';
 import { setProjectActions } from './config/projectActions';
 import { useHistory, NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import * as generalProjectActions from 'components/ProjectsCommon/logic/actions';
+import * as actions from './logic/actions';
+import OptionsWithConfirmation from 'components/ProjectsCommon/ConfirmedTrash';
 
 const Projects: React.FC = () => {
 	const history = useHistory();
 	const { t } = useTranslation();
+	const dispatch = useDispatch();
 	const { projects, isLoading } = useSelector((rootState: RootState) => rootState.projects);
+	const { isDeleted: isProjectDeleted, isLoading: isDeleting } = useSelector(
+		(rootState: RootState) => rootState.projectGeneral,
+	);
+	const [searchName, setSearchName] = useState<string>('');
 
-	const [searchName, setSearchName] = useState('');
+	if (isProjectDeleted) {
+		dispatch(generalProjectActions.resetProjectDeletingState());
+		dispatch(actions.startLoading());
+	}
 
 	const onSearch = (event: ChangeEvent<HTMLInputElement>) => {
 		const searchValue = event.target.value;
@@ -29,49 +39,57 @@ const Projects: React.FC = () => {
 		history.push(history.location.pathname + '/projectSettings/' + id);
 	};
 
+	const onTrash = (id: string): void => {
+		dispatch(generalProjectActions.startDeletingProject({ id }));
+	};
+
 	return (
-		<div className={styles.wrapper}>
-			<div className={styles.wrapper__title}>
-				<h1 className={styles.title}>{t('projects')}</h1>
-				<CreateProjectModal />
+		<>
+			<div className={styles.wrapper}>
+				<div className={styles.wrapper__title}>
+					<h1 className={styles.title}>{t('projects')}</h1>
+					<CreateProjectModal />
+				</div>
+				<div className={[styles.wrapper__filters, styles.filters].join(' ')}>
+					<Input icon="search" placeholder={t('search')} onChange={onSearch} value={searchName} />
+				</div>
+				<div className={styles.wrapper__table}>
+					<Table celled padded>
+						<Table.Header>
+							<Table.Row>
+								<Table.HeaderCell>{t('name')}</Table.HeaderCell>
+								<Table.HeaderCell>{t('key')}</Table.HeaderCell>
+								<Table.HeaderCell>{t('type')}</Table.HeaderCell>
+								<Table.HeaderCell>{t('lead')}</Table.HeaderCell>
+								<Table.HeaderCell>{t('board')}</Table.HeaderCell>
+								<Table.HeaderCell>{t('settings')}</Table.HeaderCell>
+							</Table.Row>
+						</Table.Header>
+						{isLoading || isDeleting ? null : (
+							<Table.Body>
+								{filteredData.map(({ id, name, key, lead: { firstName, lastName } }) => (
+									<Table.Row key={id}>
+										<Table.Cell>{name}</Table.Cell>
+										<Table.Cell>{key}</Table.Cell>
+										<Table.Cell>Software</Table.Cell>
+										<Table.Cell>{`${firstName} ${lastName}`}</Table.Cell>
+										<Table.Cell>
+											<NavLink to={`/project/${id}/issues`}>{t('go_to_board')}</NavLink>
+										</Table.Cell>
+										<Table.Cell>
+											<OptionsWithConfirmation
+												config={setProjectActions({ id, onOpenSettings, onTrash })}
+											/>
+										</Table.Cell>
+									</Table.Row>
+								))}
+							</Table.Body>
+						)}
+					</Table>
+					{isLoading || isDeleting ? <Spinner /> : ''}
+				</div>
 			</div>
-			<div className={[styles.wrapper__filters, styles.filters].join(' ')}>
-				<Input icon="search" placeholder={t('search')} onChange={onSearch} value={searchName} />
-			</div>
-			<div className={styles.wrapper__table}>
-				<Table celled padded>
-					<Table.Header>
-						<Table.Row>
-							<Table.HeaderCell>{t('name')}</Table.HeaderCell>
-							<Table.HeaderCell>{t('key')}</Table.HeaderCell>
-							<Table.HeaderCell>{t('type')}</Table.HeaderCell>
-							<Table.HeaderCell>{t('lead')}</Table.HeaderCell>
-							<Table.HeaderCell>{t('board')}</Table.HeaderCell>
-							<Table.HeaderCell>{t('settings')}</Table.HeaderCell>
-						</Table.Row>
-					</Table.Header>
-					{isLoading ? null : (
-						<Table.Body>
-							{filteredData.map(({ id, name, key, lead: { firstName, lastName } }) => (
-								<Table.Row key={id}>
-									<Table.Cell>{name}</Table.Cell>
-									<Table.Cell>{key}</Table.Cell>
-									<Table.Cell>Software</Table.Cell>
-									<Table.Cell>{`${firstName} ${lastName}`}</Table.Cell>
-									<Table.Cell>
-										<NavLink to={`/project/${id}/issues`}>{t('go_to_board')}</NavLink>
-									</Table.Cell>
-									<Table.Cell>
-										<Options config={setProjectActions({ id, onOpenSettings })} />
-									</Table.Cell>
-								</Table.Row>
-							))}
-						</Table.Body>
-					)}
-				</Table>
-				{isLoading ? <Spinner /> : ''}
-			</div>
-		</div>
+		</>
 	);
 };
 
