99acce9886e2fafbd83412cb07492653e3dd481d
R-Mielamud <2m.roman2@gmail.com>
2020-08-23 14:57:59 +0300

    Add cr, upd and del triggers to scrum board

--- client/src/components/Sprint/SprintIssues/index.tsx
+++ client/src/components/Sprint/SprintIssues/index.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { List, Item } from 'semantic-ui-react';
 import styles from './styles.module.scss';
 import { useTranslation } from 'react-i18next';
 import AssigneeAvatar from './AssigneeAvatar/index';
+import { useIO } from 'hooks/useIO';
 
 type Props = { issues: WebApi.Entities.Issue[] };
 
-export const SprintIssues: React.FC<Props> = ({ issues }: Props) => {
+export const SprintIssues: React.FC<Props> = ({ issues: givenIssues }: Props) => {
+	console.log(givenIssues);
+	const [issues, setIssues] = useState<WebApi.Entities.Issue[]>(givenIssues);
+	const [mustSetIssues, setMustSetIssues] = useState<boolean>(true);
 	const { t } = useTranslation();
+	const io = useIO(WebApi.IO.Types.Issue);
+
+	useEffect(() => {
+		if (mustSetIssues && givenIssues) {
+			setIssues(givenIssues);
+			setMustSetIssues(false);
+		}
+	}, [mustSetIssues, givenIssues]);
+
+	if (!io) {
+		return null;
+	}
+
+	io.on(WebApi.IO.IssueActions.CreateIssue, (newIssue: WebApi.Entities.Issue) => {
+		setIssues([...issues, newIssue]);
+	});
+
+	io.on(WebApi.IO.IssueActions.UpdateIssue, (id: string, newIssue: WebApi.Entities.Issue) => {
+		const index = issues.findIndex((issue) => issue.id === id);
+		const newIssues = [...issues];
+		newIssues[index] = newIssue;
+		setIssues(newIssues);
+	});
+
+	io.on(WebApi.IO.IssueActions.DeleteIssue, (id: string) => {
+		const index = issues.findIndex((issue) => issue.id === id);
+		const newIssues = [...issues];
+		newIssues.splice(index, 1);
+		setIssues(newIssues);
+	});
 
 	const issuesList =
 		issues?.length > 0 ? (
