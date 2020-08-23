f5129ddceba624aaf597512f8f8d949802dde3ea
R-Mielamud <2m.roman2@gmail.com>
2020-08-23 18:06:29 +0300

    Remove unnecesary class extension

--- server/src/socketConnectionHandlers/issue.handler.ts
+++ server/src/socketConnectionHandlers/issue.handler.ts
@@ -1,11 +1,5 @@
 import { ConnectionHandler } from '../../config/io.config';
 import { Types } from '../models/IO';
 
-class IssueConnectionHandler extends ConnectionHandler {
-	public constructor() {
-		super(Types.Issue);
-	}
-}
-
-const issueHandler = new IssueConnectionHandler();
+const issueHandler = new ConnectionHandler(Types.Issue);
 export default issueHandler;
