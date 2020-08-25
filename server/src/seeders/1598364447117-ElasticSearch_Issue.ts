b71ee66247c320555def08e73f7977eb601f59e9
danylokarpenko <karpenkodanylo@gmail.com>
2020-08-25 18:58:54 +0300

    Remove console.log

--- server/src/seeders/1598364447117-ElasticSearch_Issue.ts
+++ server/src/seeders/1598364447117-ElasticSearch_Issue.ts
@@ -5,8 +5,6 @@ import Elastic from '../services/elasticsearch.service';
 export class ElasticSearchIssue1598364447117 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		const issues = await getCustomRepository(IssueRepository).findAll();
-		console.log('issuesn\n\n\n\n\n\n\n\n\n', issues.length);
-
 		const elastic = new Elastic('issue');
 		await elastic.createIndexIfNotExist('issue');
 		await elastic.bulk(issues);
