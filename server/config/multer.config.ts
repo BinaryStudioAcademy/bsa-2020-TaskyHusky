d2dd0750059520b62c046dc28ac9e9bf5787ff7d
R-Mielamud <2m.roman2@gmail.com>
2020-08-28 17:45:57 +0300

    Add drag'n'drop validation + localize

--- server/config/multer.config.ts
+++ server/config/multer.config.ts
@@ -1,25 +1,19 @@
-import { generateExtnameRegex } from "../src/helpers/extnameRegex";
+import { generateExtnameRegex } from '../src/helpers/extnameRegex';
 
 export const allowedImageUploadMimeTypes = {
-    mime: [
-        'image/png',
-        'image/jpg',
-        'image/jpeg', 'image/bmp'
-    ]
+	mime: ['image/png', 'image/jpg', 'image/jpeg', 'image/bmp'],
 };
 
 export const allowedIssueAttchmentUploadFileTypes = {
-    mime: [
-        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        'image/png',
-        'image/jpeg',
-        'image/bmp',
-        'image/jpg'
-    ],
-    regex: [
-        generateExtnameRegex('txt'),
-        generateExtnameRegex('csv')
-    ]
+	mime: [
+		'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+		'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+		'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+		'image/png',
+		'image/jpeg',
+		'image/bmp',
+		'image/jpg',
+		'image/gif',
+	],
+	regex: [generateExtnameRegex('txt'), generateExtnameRegex('csv')],
 };
