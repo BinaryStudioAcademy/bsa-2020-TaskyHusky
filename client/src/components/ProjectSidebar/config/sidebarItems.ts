a502910495a6148692e3e6005be1a8eb59d5137b
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-08-29 18:57:58 +0300

    Add new sidebar styles and implementation

---
+++ client/src/components/ProjectSidebar/config/sidebarItems.ts
@@ -0,0 +1,37 @@
+export const sidebarItems = [
+	{
+		section: 'projectSettings',
+		icon: 'setting',
+		title: 'details',
+	},
+	{
+		section: 'projectPeople',
+		icon: 'street view',
+		title: 'people',
+	},
+	{
+		section: 'projectIssueTypes',
+		icon: 'check square',
+		title: 'issue_types',
+	},
+	{
+		section: 'projectVersions',
+		icon: 'random',
+		title: 'versions',
+	},
+	{
+		section: 'projectLabels',
+		icon: 'tags',
+		title: 'labels',
+	},
+	{
+		section: 'projectPermissions',
+		icon: 'key',
+		title: 'permissions',
+	},
+	{
+		section: 'projectNotifications',
+		icon: 'bell',
+		title: 'notifications',
+	},
+];
