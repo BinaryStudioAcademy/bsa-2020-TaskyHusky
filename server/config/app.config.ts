3b1aee2e43ea0e6d494ace547d4e3418adcfcc95
R-Mielamud <2m.roman2@gmail.com>
2020-08-29 21:06:41 +0300

    Fix

--- server/config/app.config.ts
+++ server/config/app.config.ts
@@ -1,4 +1,2 @@
-import { isProd } from '../src/helpers/nodeEnv';
-
 export const appPort = process.env.APP_PORT || 5000;
-export const frontendURL = isProd() ? process.env.PROD_FRONTEND_URL : process.env.DEV_OR_TEST_FRONTEND_URL;
+export const frontendURL = process.env.FRONTEND_URL;
