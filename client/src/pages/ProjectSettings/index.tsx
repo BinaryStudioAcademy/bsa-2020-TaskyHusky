0ae4f8dc588e0f03bdfd14f2f815ebb5637f78e0
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-08-30 15:21:16 +0300

    Fix routing path and render component

--- client/src/pages/ProjectSettings/index.tsx
+++ client/src/pages/ProjectSettings/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+
 import DefaultPageWrapper from 'containers/DefaultPageWrapper';
 import ProjectSettings from 'containers/ProjectSettings';
 import ProjectDetails from 'containers/ProjectDetails';
+import { SETTINGS_SECTION } from 'components/ProjectSidebar/config/sidebarItems';
+import ProjectLabels from 'containers/ProjectLabels';
 
 const ProjectSettingsPage: React.FC = () => {
+	const { section } = useParams();
+	let renderComponent;
+
+	switch (section) {
+		case SETTINGS_SECTION.details:
+			renderComponent = <ProjectDetails />;
+			break;
+		case SETTINGS_SECTION.labels:
+			renderComponent = <ProjectLabels />;
+			break;
+		default:
+			renderComponent = <Redirect to={'/404'} />;
+	}
+
 	return (
 		<DefaultPageWrapper isOverflowHidden>
-			<ProjectSettings>
-				<ProjectDetails />
-			</ProjectSettings>
+			<ProjectSettings>{renderComponent}</ProjectSettings>
 		</DefaultPageWrapper>
 	);
 };
