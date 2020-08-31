1438baa801378556f36215d10106913457434fbd
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-09-01 01:31:36 +0300

    Add project settings sections

--- client/src/pages/ProjectSettings/index.tsx
+++ client/src/pages/ProjectSettings/index.tsx
@@ -5,6 +5,8 @@ import DefaultPageWrapper from 'containers/DefaultPageWrapper';
 import ProjectSettings from 'containers/ProjectSettings';
 import ProjectDetails from 'containers/ProjectDetails';
 import { SETTINGS_SECTION } from 'components/ProjectSidebar/config/sidebarItems';
+import ProjectPeople from 'containers/ProjectPeople';
+import ProjectLabels from 'containers/ProjectLabels';
 
 const ProjectSettingsPage: React.FC = () => {
 	const { section } = useParams();
@@ -13,6 +15,12 @@ const ProjectSettingsPage: React.FC = () => {
 	switch (section) {
 		case SETTINGS_SECTION.details:
 			renderComponent = <ProjectDetails />;
+			break;
+		case SETTINGS_SECTION.people:
+			renderComponent = <ProjectPeople />;
+			break;
+		case SETTINGS_SECTION.labels:
+			renderComponent = <ProjectLabels />;
 			break;
 		default:
 			renderComponent = <Redirect to={'/404'} />;
