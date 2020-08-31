c385c959b327658fc91ad363f012833005a37d3d
kostia1359 <tzimbal.k@gmail.com>
2020-08-31 23:17:50 +0300

    fixed http problem tmp

--- server/src/helpers/emailTemplates.helper.ts
+++ server/src/helpers/emailTemplates.helper.ts
@@ -25,7 +25,7 @@ export const resetPasswordTemplate = (token: string) => {
     <div style="font-family:arial,'helvetica neue',helvetica,sans-serif;padding:0;width:600px;margin:0 auto;background:#ffffff!important">
 		  <h1 style="min-width:100%;width:100%;padding-top:30px;padding-right:0px;padding-bottom:0px;padding-left:0px">Notification from Tasky-Husky Team &#128527;</h1>
       <div style="height:2px;border-bottom:2px solid #e5e5e5"></div>
-      <p style="font-size:20px;font-weight:300;letter-spacing:1px">Hi there! We received a request to reset the password for the Tasky-Husky account associated with this e-mail address. Click the link below to reset your password using our secure server:
+      <p style="font-size:20px;font-weight:300;letter-spacing:1px">${frontendURL} Hi there! We received a request to reset the password for the Tasky-Husky account associated with this e-mail address. Click the link below to reset your password using our secure server:
 			  <a style="letter-spacing:0px" href=${frontendURL}/reset-password/${token}>Link to change password</a></p>
 		  <p style="font-size:20px;font-weight:300;letter-spacing:1px">If clicking the link doesn't work, you can copy and paste the link into your web browser's address bar. You will be able to update password for your Tasky-Husky account after clicking the link above.
       If you did not request to have your password changed, you can ignore this email.</p>
