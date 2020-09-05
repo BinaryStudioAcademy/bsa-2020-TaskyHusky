61a466bceaa3185b25b6d65dc04b46d87cbad7db
Vitaliy Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-09-05 13:25:27 +0300

    Change project labels list styles

--- client/src/components/common/Label/index.tsx
+++ client/src/components/common/Label/index.tsx
@@ -1,33 +1,18 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './styles.module.scss';
-import useLabelWidth from './useLabelWidth';
 
 interface Props {
 	text: string;
 	textColor: string;
 	backgroundColor: string;
-	isFullSize?: boolean;
 }
 
-const Label: React.FC<Props> = ({ text, textColor, backgroundColor, isFullSize }) => {
-	const { isBlock, label, labelContainer } = useLabelWidth({ text });
-
+const Label: React.FC<Props> = ({ text, textColor, backgroundColor }) => {
 	return (
-		<span ref={labelContainer} className={styles.wrapper}>
-			<span
-				ref={label}
-				className={classNames(
-					{ [styles.container__label_block]: isBlock },
-					{ [styles.full_size__container]: isFullSize },
-					{ [styles.container]: text },
-				)}
-				style={{ backgroundColor }}
-			>
-				<span
-					className={classNames(styles.label, { [styles.full_size]: isFullSize })}
-					style={{ color: textColor }}
-				>
+		<span className={styles.wrapper}>
+			<span className={classNames({ [styles.container]: text })} style={{ backgroundColor }}>
+				<span className={styles.label} style={{ color: textColor }}>
 					{text}
 				</span>
 			</span>
