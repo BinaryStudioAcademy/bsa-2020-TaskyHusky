df5545b43a0cf5e3ff91b38cdc1cece9d0f28068
Alesya103 <alesya.afanaseva27@gmail.com>
2020-09-06 20:42:30 +0300

    fix functionality; modals team page

--- client/src/components/TeamLinks/DeleteLink.tsx
+++ client/src/components/TeamLinks/DeleteLink.tsx
@@ -15,9 +15,9 @@ const DeleteLink = ({ onClose, link, onDelete }: Props) => {
 
 	return (
 		<Modal onClose={onClose} open size="tiny">
-			<Modal.Header>{t('delete_link')}</Modal.Header>
+			<Modal.Header className="standartHeader">{t('delete_link')}</Modal.Header>
 			<Modal.Content>
-				<p className={styles.linkDescription}>
+				<p className="textData">
 					{t('link_to')} <span className={styles.linkTitle}>{link.name}</span>{' '}
 					{t('will_be_deleted_from_section')}
 				</p>
@@ -25,12 +25,12 @@ const DeleteLink = ({ onClose, link, onDelete }: Props) => {
 			<Modal.Actions>
 				<Button
 					content={t('accept')}
-					className={styles.editBtn}
+					className="primaryBtn"
 					labelPosition="left"
 					icon="checkmark"
 					onClick={() => onDelete(link)}
 				/>
-				<Button className={styles.cancelBtn} onClick={onClose}>
+				<Button className="cancelBtn" onClick={onClose}>
 					{t('cancel')}
 				</Button>
 			</Modal.Actions>
