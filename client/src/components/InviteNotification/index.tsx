f3886e72efdfb4d6829570ae5422ab894abf9906
R-Mielamud <2m.roman2@gmail.com>
2020-09-06 17:24:35 +0300

    Redesign notifications

--- client/src/components/InviteNotification/index.tsx
+++ client/src/components/InviteNotification/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Card, Image, Icon } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
 import { useTranslation } from 'react-i18next';
 import * as actions from '../../containers/Header/logic/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../typings/rootState';
+import styles from './styles.module.scss';
 
 interface Props {
 	id: string;
@@ -38,29 +39,32 @@ const InviteNotification: React.FC<Props> = (props: Props) => {
 		);
 	};
 
+	const [firstName, lastName] = name.split(' ');
+	const firstLetter = firstName ? firstName[0] : '';
+	const lastLetter = lastName ? lastName[0] : '';
+	const initials = firstLetter + lastLetter;
+
 	return (
-		<Card>
-			<Card.Content>
-				<Image floated="right" size="mini" src={avatar} circular />
-				<Card.Header>
+		<div className={styles.notification}>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				{avatar ? (
+					<img src={avatar} className={styles.avatar} style={{ float: 'left' }} alt="Avatar" />
+				) : (
+					<div className={styles.avatar}>{initials}</div>
+				)}
+				<div>
 					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
 						<div>{name}</div>
 						<div>
-							<Icon
-								name="check"
-								style={{ color: '#deae0f' }}
-								onClick={handleApprove}
-								title={t('approve')}
-								link
-							/>
+							<Icon name="check" onClick={handleApprove} title={t('approve')} link />
 							<Icon name="close" onClick={handleDecline} title={t('decline')} link />
 						</div>
 					</div>
-				</Card.Header>
-				<Card.Meta style={{ marginTop: 5 }}>{jobTitle}</Card.Meta>
-				<Card.Description>{t('invite_notification_text')}</Card.Description>
-			</Card.Content>
-		</Card>
+					<div className={styles.meta}>{jobTitle}</div>
+				</div>
+			</div>
+			<div style={{ marginTop: 10 }}>{t('invite_notification_text')}</div>
+		</div>
 	);
 };
 
