061077388f17d2f8708e545e4d53c7d4ca0eabc7
R-Mielamud <2m.roman2@gmail.com>
2020-09-08 14:35:27 +0300

    Improve issues page

--- client/src/components/IssueCard/index.tsx
+++ client/src/components/IssueCard/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Segment, Header, Icon } from 'semantic-ui-react';
+import { Segment, Header, Icon, Label } from 'semantic-ui-react';
 import { Draggable } from 'react-beautiful-dnd';
 import styles from './styles.module.scss';
 import { Redirect } from 'react-router-dom';
 import { getUsername } from 'helpers/getUsername.helper';
 import UserAvatar from 'components/common/UserAvatar';
+import { SemanticCOLORS } from 'semantic-ui-react/dist/commonjs/generic';
 
 interface BaseEvent {
 	id: string;
@@ -60,11 +61,42 @@ const IssueCard: React.FC<Props> = ({ issue, index, noDrag, noRedirect, selectab
 	const content = (
 		<Segment
 			onClick={onClick}
-			style={{ backgroundColor: selected ? '#ffeeb4' : 'white' }}
+			style={{ backgroundColor: selected ? '#eee' : 'white' }}
 			className={styles.card_margin}
 		>
 			{redirecting ? <Redirect to={`/issue/${issue.issueKey}`} /> : ''}
-			<Header>{issue.summary}</Header>
+			<div>
+				{issue.labels
+					? issue.labels.map((label) => (
+							<Label
+								key={label.id}
+								style={{
+									color: label.textColor,
+									backgroundColor: label.backgroundColor,
+									paddingTop: 3,
+									paddingBottom: 3,
+								}}
+							>
+								{label.text}
+							</Label>
+					  ))
+					: ''}
+			</div>
+			<Header style={{ marginBottom: 0, marginTop: 10 }}>
+				<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+					<div>
+						{issue.summary}
+						{issue.storyPoint ? <Label content={issue.storyPoint} /> : ''}
+					</div>
+					<div>
+						{issue.status ? (
+							<Label color={issue.status.color as SemanticCOLORS} content={issue.status.title} />
+						) : (
+							''
+						)}
+					</div>
+				</div>
+			</Header>
 			<div className={styles.inlineContainer}>
 				<div>
 					<Icon name={issue.type.icon as any} color={issue.type.color as any} title={issue.type.title} />
