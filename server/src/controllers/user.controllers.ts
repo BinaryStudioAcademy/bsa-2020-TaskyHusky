d5817117130074de0155320ff63c86a779e16ae7
Alesya103 <alesya.afanaseva27@gmail.com>
2020-09-11 18:08:57 +0300

    aux

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-import { v4 } from 'uuid';
 import { getCustomRepository } from 'typeorm';
 import { randomBytes } from 'crypto';
 import { passwordValid, hashPassword } from '../helpers/password.helper';
@@ -45,7 +44,8 @@ class UserController {
 	uploadAvatar = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 		const userRepository = getCustomRepository(UserRepository);
 		const { id, firstName, lastName } = <UserProfile>req.user;
-		const name = `${firstName}_${lastName}_${id}_${v4()}`;
+		const timestamp = +new Date();
+		const name = `${firstName}_${lastName}_${id}_${timestamp}`;
 		try {
 			const avatar = await uploadS3(avatarFolder, req.file, name);
 			if (!avatar) {
