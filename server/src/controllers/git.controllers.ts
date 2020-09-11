cb0581e28386b42470b7695085882f8d0e2620ca
Alesya103 <alesya.afanaseva27@gmail.com>
2020-09-11 18:42:11 +0300

    aux

--- server/src/controllers/git.controllers.ts
+++ server/src/controllers/git.controllers.ts
@@ -9,26 +9,15 @@ class GitController {
 	getCommits = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 		const { id } = req.params;
 		const repository = getCustomRepository(IssueRepository);
-
 		try {
 			const issue = await repository.findOneById(id);
 			const { issueKey, links } = issue;
 			if (!issueKey) {
 				throw new Error('Could not find such issue');
 			}
-			const promises = links?.map(async (link) => {
-				const commits = await getMessages(issueKey, link);
-				return commits;
-			});
-			if (!promises) {
-				throw new Error('Error connect with git');
-			}
-			const commits = await Promise.all(promises);
+			const commits = await getMessages(issueKey);
 
-			const parsedCommits = commits.reduce((flatten, toFlatten) =>
-				flatten.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten),
-			);
-			res.send(parsedCommits);
+			res.send(commits);
 		} catch (error) {
 			next(new ErrorResponse(HttpStatusCode.NOT_FOUND, error.message));
 		}
